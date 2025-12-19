3f5b7ff12298c84dacebc71df014f871c9850bc1
Achbelsdn <achbelsdn@gmail.com>
2025-12-19 11:48:03 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ const { existsSync } = require('fs')
 if (existsSync('config.env')) require('dotenv').config({ path: './config.env' })
 
 module.exports = { 
-    SESSION_ID: process.env.SESSION_ID || '',
-    OWNER_NUMBER: process.env.OWNER_NUMBER || "2349067339193",
-    WORKTYPE: process.env.WORKTYPE || "private",
+    SESSION_ID: process.env.SESSION_ID || 'kord_ai-pV4uGoDzQFHljfMN',
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "22967535294",
+    WORKTYPE: process.env.WORKTYPE || "public",
     PREFIX: process.env.PREFIX || "[.]",
     ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || true,
     MONGODB_URI: process.env.MONGODB_URI || "",
